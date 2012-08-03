4f53ce5cfd7341c0d793b97d04934a9089e79189
MPogoda <do0dlez1991@gmail.com>
2012-08-03 22:06:17 +0300

    supplied class Light

---
+++ Light.java
@@ -0,0 +1,11 @@
+package ua.patterns;
+
+public class Light {
+  public void on() {
+    System.out.println("LIGHTS ON!");
+  }
+
+  public void off() {
+    System.out.println("LIGHTS OFF!");
+  }
+}
